325b1118fefc2ce9ada76953e67ffd6c526e9c45
shri142 <shrikrishna2002umbare@gmail.com>
2022-07-22 03:00:49 +0530

    modified admin panel

--- music/musicbeats/models.py
+++ music/musicbeats/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    # song_id = models.AutoField(True)  unable to makemigrations and migrate //solve it after
+    # song_id = models.AutoField(primary_key=True ) # unable to makemigrations and migrate //solve it after
     name = models.CharField(max_length=2000)
     singer = models.CharField(max_length=2000)
     tags = models.CharField(max_length=100)
     image = models.ImageField(upload_to='documents')
     song = models.FileField(upload_to='documents')
+    # movie = models.CharField(max_length=150, default="None")
