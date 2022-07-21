b6c34c2c8477cec0c3f9b148ad93666fedab12ef
shri142 <shrikrishna2002umbare@gmail.com>
2022-07-22 02:30:34 +0530

    Update db.sqlite

--- music/musicbeats/models.py
+++ music/musicbeats/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    # song_id = models.AutoField(Primary_key=True)
+    # song_id = models.AutoField(True)  unable to makemigrations and migrate //solve it after
     name = models.CharField(max_length=2000)
     singer = models.CharField(max_length=2000)
     tags = models.CharField(max_length=100)
