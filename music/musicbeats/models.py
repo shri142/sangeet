ba900921272ec3952a7aad51f121486b0f854dfe
shri142 <shrikrishna2002umbare@gmail.com>
2022-07-22 02:25:47 +0530

    Update models.py

--- music/musicbeats/models.py
+++ music/musicbeats/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    song_id = models.AutoField(Primary_key=True)
+    # song_id = models.AutoField(Primary_key=True)
     name = models.CharField(max_length=2000)
-    singer= models.CharField(max_length=2000)
-    tags=models.CharField(max_length=100)
-    image=models.ImageField(upload_to=2000)
+    singer = models.CharField(max_length=2000)
+    tags = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='documents')
+    song = models.FileField(upload_to='documents')
